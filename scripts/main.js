f498d9724f2e4449b2210f4562dd6dc03ce60d68
Enzo <35852814+EnzoMatrixNL@users.noreply.github.com>
2025-04-27 15:50:23 +0200

    simplified css

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const hexagon = document.getElementById('hexagonHamburger');
+    const hexagon = document.getElementById('hexagon-00');
     if (hexagon) {
         hexagon.addEventListener('click', function () {
             hexagon.classList.toggle('active');
